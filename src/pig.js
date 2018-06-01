67453d1343c1c96195f040a2beee675d95386e71
Oliver Hamlet <oliver.hamlet@gmail.com>
2018-06-01 20:58:16 +0100

    Support scrolling from non-window elements
    Add a scroller option that is window by default but which can be
    set to an HTML element (such as the parent of the grid container).

--- src/pig.js
+++ src/pig.js
@@ -199,6 +199,13 @@
       containerId: 'pig',
 
       /**
+       * Type: window | HTMLElement
+       * Default: window
+       * Description: The window or HTML element that the grid scrolls in.
+       */
+      scroller: window,
+
+      /**
        * Type: string
        * Default: 'pig'
        * Description: The prefix associated with this library that should be
@@ -321,6 +328,8 @@
     if (!this.container) {
       console.error('Could not find element with ID ' + this.settings.containerId);
     }
+
+    this.scroller = this.settings.scroller;
 
     // Our global reference for images in the grid.  Note that not all of these
     // images are necessarily in view or loaded.
@@ -587,7 +596,7 @@
 
     // Now we compute the location of the top and bottom buffers:
     var containerOffset = _getOffsetTop(this.container);
-    var windowHeight = window.innerHeight;
+    var scrollerHeight = this.scroller === window ? window.innerHeight : this.scroller.offsetHeight;
 
     // This is the top of the top buffer. If the bottom of an image is above
     // this line, it will be removed.
@@ -595,7 +604,7 @@
 
     // This is the bottom of the bottom buffer.  If the top of an image is
     // below this line, it will be removed.
-    var maxTranslateY = this.latestYOffset - containerOffset + windowHeight + bufferBottom;
+    var maxTranslateY = this.latestYOffset - containerOffset + scrollerHeight + bufferBottom;
 
     // Here, we loop over every image, determine if it is inside our buffers or
     // no, and either insert it or remove it appropriately.
@@ -634,7 +643,7 @@
     var onScroll = function() {
       // Compute the scroll direction using the latestYOffset and the
       // previousYOffset
-      var newYOffset = window.pageYOffset;
+      var newYOffset = _this.scroller === window ? window.pageYOffset : _this.scroller.scrollTop;
       _this.previousYOffset = _this.latestYOffset || newYOffset;
       _this.latestYOffset = newYOffset;
       _this.scrollDirection = (_this.latestYOffset > _this.previousYOffset) ? 'down' : 'up';
@@ -660,14 +669,15 @@
    */
   Pig.prototype.enable = function() {
     this.onScroll = this._getOnScroll();
-    window.addEventListener('scroll', this.onScroll);
+
+    this.scroller.addEventListener('scroll', this.onScroll);
 
     this.onScroll();
     this._computeLayout();
     this._doLayout();
 
     optimizedResize.add(function() {
-      this.lastWindowWidth = window.innerWidth;
+      this.lastWindowWidth = this.scroller === window ? window.innerWidth : this.scroller.offsetWidth;
       this._computeLayout();
       this._doLayout();
     }.bind(this));
@@ -681,7 +691,7 @@
    * @returns {object} The Pig instance.
    */
   Pig.prototype.disable = function() {
-    window.removeEventListener('scroll', this.onScroll);
+    this.scroller.removeEventListener('scroll', this.onScroll);
     optimizedResize.disable();
     return this;
   };
