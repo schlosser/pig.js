7e3af66be4bb373c60eda7a41796664cbfd9e691
Bernhard Pottler <bpottler@nexgo.de>
2021-10-10 06:35:06 +0200

    feature: remove css support for IE<8
    Market share of IE is less than 1% and IE does not support classes
    'style.type' is deprecated and today should be avoided

--- src/pig.js
+++ src/pig.js
@@ -109,14 +109,7 @@
 
     const head = document.head || document.getElementsByTagName('head')[0];
     const style = document.createElement('style');
-
-    style.type = 'text/css';
-    if (style.styleSheet) {
-      // set style for IE8 and below
-      style.styleSheet.cssText = css;
-    } else {
-      style.appendChild(document.createTextNode(css));
-    }
+    style.appendChild(document.createTextNode(css));
 
     head.appendChild(style);
   }
